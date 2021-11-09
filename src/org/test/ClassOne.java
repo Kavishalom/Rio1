53b9b3eeded85b94a9a85a49332df69a8e608886
Kavishalom <prakashbabu95@gmail.com>
2021-11-09 08:21:45 -0800

    conflict resolved

--- src/org/test/ClassOne.java
+++ src/org/test/ClassOne.java
@@ -2,4 +2,10 @@ package org.test;
 
 public class ClassOne {
 
+	private static void passWord() {
+		System.out.println("password");
+			}
+			public static void main(String[] args) {
+				passWord();
+			}
 }
